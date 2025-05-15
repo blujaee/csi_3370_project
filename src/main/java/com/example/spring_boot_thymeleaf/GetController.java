748e7a90b62780419abfc61fb12fb3db4f8dc75f
chammasj <jchammas@oakland.edu>
2025-05-15 14:55:21 -0400

    Create branch for springboot/thymeleaf testing

---
+++ src/main/java/com/example/spring_boot_thymeleaf/GetController.java
@@ -0,0 +1,34 @@
+package com.example.spring_boot_thymeleaf;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GetController {
+    // GetMapping("/") -> Redirects root of the webserver / to example(.html) for all GET requests
+    // See .../main/resources/templates
+    @GetMapping("/")
+    public String example(Model model) {
+        model.addAttribute("test", "This is coming from the controller");
+        model.addAttribute("people", Arrays.asList(
+            new Person("Bob", 20, "bob.lob@gmail.com"), 
+            new Person("Jane", 18, "jane.doe@gmail.com"),
+            new Person("Larry", 23, "larry123@gmail.com")
+        ));
+
+        return "example";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+}
