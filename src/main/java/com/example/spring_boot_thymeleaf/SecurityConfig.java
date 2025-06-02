e69d3df8f95a2f0f7e66762e014e1fbb2f907fff
audrey ˚ʚ♡ɞ˚ <119465635+blujaee@users.noreply.github.com>
2025-06-02 12:27:56 -0400

    force custom authentication provider

--- src/main/java/com/example/spring_boot_thymeleaf/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_thymeleaf/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.spring_boot_thymeleaf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -11,12 +12,23 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public AuthenticationProvider customAuthenticationProvider() {
+        return new CustomAuthenticationProvider();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+          .authenticationProvider(customAuthenticationProvider())
           .authorizeHttpRequests(auth -> auth
               .requestMatchers(
-                  "/login",      
-                  "/register",      
+                  "/login",
+                  "/register",
                   "/css/**", "/js/**", "/images/**"
               ).permitAll()
               .anyRequest().authenticated()
@@ -36,5 +48,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
